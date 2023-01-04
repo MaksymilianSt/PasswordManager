25d18c46b23b1e48a06247bba6671fe6e322cf69
MaksymilianSt <Maksymilianst02@gmail.com>
2023-01-04 17:21:06 +0100

    doxy added

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Maximiliano/Desktop/ProjektHaslo
+# Install script for directory: C:/Users/Maximiliano/Desktop/PasswordManager
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Maximiliano/Desktop/ProjektHaslo/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Maximiliano/Desktop/PasswordManager/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
